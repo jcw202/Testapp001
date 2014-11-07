e39d41d79c26a681b0ea0202c29f98f7917b114c
jcw202 <jennifer.warner@phe.gov.uk>
2014-11-07 13:52:16 +0000

    auth.py

---
+++ auth.py
@@ -0,0 +1,11 @@
+
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+	consumer_key="9jUxgLEaRifhgysi18Wdm21hY"
+	consumer_secret="soOgcxNAbg9HEMDuYW48GYZ9VUOIhjikza0wjYV089DrYyauFK"
+
+	# After the step above, you will be redirected to your app's page.
+	# Create an access token under the the "Your access token" section
+	access_token="519680897-QbWKirxTE1jdH8fJWsaYpNelSxC51wYJP28ttH5G"
+	access_token_secret="uyJun88Kh3OP01Mc4spPYMce6Akt5f0epTuNQWa8oQljs"
